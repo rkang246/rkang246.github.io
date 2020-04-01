f597c94cd2da7f22b9d62620e78afd1b657856c8
rqkang <rqkang@umich.edu>
2020-04-01 17:02:22 -0400

    Added more comments, cleaned up experience links

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,18 +1,17 @@
-var acc = document.getElementsByClassName("exp-accordion");
-var i;
+/*
+Experience accordion
+*/
+$('.panel-collapse').on('show.bs.collapse', function () {
+   $(this).siblings('.panel-heading').addClass('active');
+ });
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("exp-active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
+ $('.panel-collapse').on('hide.bs.collapse', function () {
+   $(this).siblings('.panel-heading').removeClass('active');
+ });
 
+/*
+Tagline Cycler
+*/
 var words = (function(){
   var words = [
       'reading <strong><a href="https://mitpress.mit.edu/books/free-will" target="_blank">free will</a></strong> by mark balaguer',
@@ -22,6 +21,9 @@ var words = (function(){
       'staying <strong>hydrated</strong>',
       'going on a <strong>run</strong>',
       'managing <strong>student debt</strong>',
+      'making this site <strong>mobile friendly</strong>',
+      'figuring out <strong>responsive</strong> design',
+      'attending <strong>office hours</strong> on zoom',
 
       ],
     el = document.querySelector('.text-cycle'),
@@ -94,10 +96,25 @@ var words = (function(){
 words.init();
 
 
-$('.panel-collapse').on('show.bs.collapse', function () {
-   $(this).siblings('.panel-heading').addClass('active');
- });
 
- $('.panel-collapse').on('hide.bs.collapse', function () {
-   $(this).siblings('.panel-heading').removeClass('active');
- });
+/* @TODO Light Switch
+ function lightSwitch() {
+   // Get the checkbox
+   var checkBox = document.getElementById("light-switch");
+
+
+   if (checkBox.checked == false) { //Light Mode
+     console.log("light");
+     document.documentElement.style.setProperty('--accent-color', '#11c973');
+     document.documentElement.style.setProperty('--inactive-color', '#444452');
+     document.documentElement.style.setProperty('--background-color', '#FFF');
+     document.documentElement.style.setProperty('--body-color', '#444452');
+   } else { //Dark Mode
+     console.log("dark");
+     document.documentElement.style.setProperty('--accent-color', '#11c973');
+     document.documentElement.style.setProperty('--inactive-color', '#FFF');
+     document.documentElement.style.setProperty('--background-color', '#15181a');
+     document.documentElement.style.setProperty('--body-color', '#949799');
+   }
+ }
+ */
