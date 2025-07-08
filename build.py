dbdf545904ffa70c6652a17017d405d84537b785
Robert Kang <rkang246@gmail.com>
2025-07-08 01:09:23 -0700

    Add support for title and caption on photo gallery

--- build.py
+++ build.py
@@ -19,6 +19,15 @@ GALLERY_IDS = [
 PHOTOS_PER_PAGE = 15
 OUTPUT_DIR = "_includes/photos"
 
+class ImgurImage:
+    def __init__(self, url, description):
+        self.url = url
+        self.description = description
+
+    def __repr__(self):
+        return f"ImgurImage(url={self.url}, description={self.description})"
+
+# Returns list of ImgurImage from imgur API
 def call_gallery_api(client_id, gallery_id):
     url = f"https://api.imgur.com/3/album/{gallery_id}/images"
     headers = {"Authorization": f"Client-ID {client_id}"}
@@ -30,21 +39,36 @@ def call_gallery_api(client_id, gallery_id):
         return []
     
     photos = data.get("data", [])
-    urls = [photo["link"] for photo in photos if "link" in photo]
+    print(data)
+    imgur_images = [(photo["link"], photo["description"]) for photo in photos if "link" in photo]
     
-    return urls
+    return imgur_images
 
-def fetch_image_urls():
-    urls = []
+def fetch_imgur_images():
+    imgur_images = []
     for gallery_id in GALLERY_IDS:
         batch = call_gallery_api(IMGUR_CLIENT_ID, gallery_id)
         if len(batch) == 0:
             exit(1)
-        urls.extend(batch)
-    return urls
+        imgur_images.extend(batch)
+    return imgur_images
+
+# Return (title, location)
+# Description is expected to be "title,sublocation,location", "sublocation,location", or empty
+def parse_description(description):
+    if description is None:
+        return None
+
+    description = description.split(",")
+    if len(description) == 2:
+        return ("", f"{description[0].strip()}, {description[1].strip()}")
+    elif len(description) == 3:
+        return (description[0].strip(), f"{description[1].strip()}, {description[2].strip()}")
+    
+    return None
 
 # build _includes/photos
-def write_photogrid_batch(batch_urls, batch_num):
+def write_photogrid_batch(batch_images, batch_num):
     if batch_num == 1:
         filename = "photogrid.html"
     else:
@@ -53,23 +77,53 @@ def write_photogrid_batch(batch_urls, batch_num):
     filepath = os.path.join(OUTPUT_DIR, filename)
 
     photogrid_html = []
-    for url in batch_urls:
+    for image in batch_images:
+        url = image[0]
+        
         photogrid_html.append('  <div class="grid__item">')
-        if url.endswith('.mp4'):
-            photogrid_html.append('    <video autoplay loop muted playsinline>')
-            photogrid_html.append(f'      <source src="{url}" type="video/mp4">')
-            photogrid_html.append('      Your browser does not support the video tag.')
-            photogrid_html.append('    </video>')
-        else:
-            photogrid_html.append(f'    <img src="{url}" loading="lazy" alt="">')
-        photogrid_html.append('  </div>')
 
+        # Build media element (image vs video)
+        if url.endswith('.mp4'):
+            media_element = [
+                '      <video autoplay loop muted playsinline>',
+                f'        <source src="{url}" type="video/mp4">',
+                '        Your browser does not support the video tag.',
+                '      </video>'
+            ]
+        else:
+            media_element = [f'      <img src="{url}" loading="lazy" alt="">']
+        
+        # Build photo caption (if exists)
+        description = parse_description(image[1])
+        # If no description, output media element directly
+        if description is None:
+            media_element = [s[2:] if s.startswith('  ') else s for s in media_element] # trim 2 spaces for formatting
+            photogrid_html.extend(media_element)
+        else:
+            title, location = description
+
+            photogrid_html.append('    <label class="click-toggle">')
+            photogrid_html.append('      <input type="checkbox" hidden>')
+            photogrid_html.extend(media_element)
+
+            photogrid_html.append('      <span class="caption">')
+            if title:
+                photogrid_html.append(f'        <span class="title">{title}</span>')
+            photogrid_html.append('        <span class="location">')
+            photogrid_html.append('          <i class="fa-solid fa-location-dot"></i>')
+            photogrid_html.append(f'          {location}')
+            photogrid_html.append('        </span>')
+            photogrid_html.append('      </span>')
+
+            photogrid_html.append('    </label>')
+
+        photogrid_html.append('  </div>')
 
     # Overwrite content into jekyll template
     with open(filepath, "w", encoding="utf-8") as file:
         file.write("\n".join(photogrid_html))
 
-    print(f"Generated {filename} with {len(batch_urls)} items.")
+    print(f"Generated {filename} with {len(batch_images)} items.")
 
 # build photo-html wrappers
 def write_photogrid_wrapper(batch_num):
@@ -97,18 +151,18 @@ def main():
         os.makedirs(OUTPUT_DIR)
 
     # Fetch all URLs
-    urls = fetch_image_urls()
-    print(f"Successfully loaded {len(urls)} from imgur api")
+    images = fetch_imgur_images()
+    print(f"Successfully loaded {len(images)} from imgur api")
 
-    total = len(urls)
+    total = len(images)
     num_pages = math.ceil(total / PHOTOS_PER_PAGE) # Calculate number of pages to divide
 
     # Create photogrid html in batch pages for pagination
     for page in range(num_pages):
         start = page * PHOTOS_PER_PAGE
         end = start + PHOTOS_PER_PAGE
-        batch_urls = urls[start:end]
-        write_photogrid_batch(batch_urls, page + 1)
+        batch_images = images[start:end]
+        write_photogrid_batch(batch_images, page + 1)
         write_photogrid_wrapper(page + 1)
     print(f"Successfully built {num_pages} photogrid files from {total} images/videos.")
 
